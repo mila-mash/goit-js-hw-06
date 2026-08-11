be0c3a292781613d1570ec8a4976e18f92ae84db
your_name <lsmaylik12@gmail.com>
2026-08-11 23:06:42 +0300

    me 6th js hw

---
+++ js/task-2.js
@@ -0,0 +1,32 @@
+class Storage {
+  #items;
+  constructor(array) {
+    this.#items = array;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    if (this.#items.indexOf(itemToRemove) != -1) {
+      this.#items.splice(this.#items.indexOf(itemToRemove), 1);
+    }
+  }
+}
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem('Scaner');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
